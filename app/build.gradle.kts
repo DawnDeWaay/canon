e13a89aea4321c794f52d95fe77efa9ab3662a36
dawndewaay <dawndewaay@gmail.com>
2026-05-10 22:39:43 -0500

    Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha01")
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
