f32181c47c5ae6ca250ad9a4666966f7eec224d7
dawndewaay <dawndewaay@gmail.com>
2026-07-10 17:50:06 -0500

    Prog

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Star from './components/Star';
+import { Button } from '@vercel/geistcn/components';
 
 export default function Home() {
   return (
     <div className='flex flex-col flex-1 items-center justify-center'>
       <main className='flex flex-wrap flex-row flex-start w-full max-w-3xl'>
         <Star color='#00ffff' />
+        <Button>Upload</Button>
       </main>
     </div>
   );
