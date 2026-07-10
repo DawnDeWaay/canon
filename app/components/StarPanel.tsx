321be43d37c6432efddfce948fc7a4297947cc6c
dawndewaay <dawndewaay@gmail.com>
2026-07-10 17:59:34 -0500

    hm

--- app/components/StarPanel.tsx
+++ app/components/StarPanel.tsx
@@ -1,4 +1,4 @@
-import Star from './Star';
+import Star from "./Star";
 
 const StarPanel = () => {
   return <Star color='#ddd' />;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+'use client';
+
+import { Button } from '@mui/material';
 import Star from './components/Star';
-import { Button } from '@vercel/geistcn/components';
+import { useColor } from './context/ColorContext';
 
 export default function Home() {
+  const { color: contextColor } = useColor();
   return (
     <div className='flex flex-col flex-1 items-center justify-center'>
       <main className='flex flex-wrap flex-row flex-start w-full max-w-3xl'>
         <Star color='#00ffff' />
-        <Button>Upload</Button>
+        <div style={{ color: contextColor, backgroundColor: contextColor }}>
+          <Button color='inherit' onClick={() => console.log('Upload clicked')} size='large'>
+            Sign-in
+          </Button>
+        </div>
       </main>
     </div>
   );
