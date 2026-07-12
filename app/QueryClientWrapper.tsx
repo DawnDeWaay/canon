1edd0e7a41f16770cb465cc2a6de61c1d95d68b0
DonDeWaay <dondewaay@gmail.com>
2026-07-12 15:48:49 -0500

    Fixed query

--- app/QueryClientWrapper.tsx
+++ app/QueryClientWrapper.tsx
@@ -1,22 +1,26 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 const QueryClientWrapper = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 1000 * 60 * 5,
-        gcTime: 1000 * 60 * 30,
-        refetchOnWindowFocus: false,
-        refetchOnReconnect: true,
-        retry: 2,
-      },
-      mutations: {
-        retry: 1,
-      },
-    },
-  });
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+            gcTime: 1000 * 60 * 30,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: true,
+            retry: 2,
+          },
+          mutations: {
+            retry: 1,
+          },
+        },
+      })
+  );
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
 
