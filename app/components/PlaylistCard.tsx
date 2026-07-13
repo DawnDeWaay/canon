733b266d1b699db0827398103eb6a7403c3495c7
dawndewaay <dawndewaay@gmail.com>
2026-07-12 20:38:14 -0500

    hm

--- app/components/PlaylistCard.tsx
+++ app/components/PlaylistCard.tsx
@@ -17,8 +17,8 @@ const PlaylistCard = ({
     <motion.div
       className='w-full rounded-lg flex items-center justify-center cursor-pointer'
       initial={false}
-      animate={{ height: '4rem', opacity: 0.9, backgroundColor: '#121212' }}
-      whileHover={{ height: '4.5rem' }}
+      animate={{ height: '4.5rem', opacity: 0.9, backgroundColor: '#121212' }}
+      whileHover={{ height: '5rem' }}
       onClick={() => setMode({ type: 'playlist', id })}
     >
       <motion.div className='flex flex-row items-center justify-between w-full h-full gap-4 px-3'>
