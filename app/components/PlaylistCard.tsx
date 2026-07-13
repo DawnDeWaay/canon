b0a78a586251ae4f169a7395930027e3e19b1e61
dawndewaay <dawndewaay@gmail.com>
2026-07-12 20:18:49 -0500

    Spacing

--- app/components/PlaylistCard.tsx
+++ app/components/PlaylistCard.tsx
@@ -17,11 +17,11 @@ const PlaylistCard = ({
     <motion.div
       className='h-14 w-full rounded-lg flex items-center justify-center cursor-pointer'
       initial={false}
-      animate={{ height: '3.5rem', opacity: 0.9, backgroundColor: '#121212' }}
-      whileHover={{ height: '4rem' }}
+      animate={{ padding: '0.5rem 0.5rem', opacity: 0.9, backgroundColor: '#121212' }}
+      whileHover={{ padding: '0.7rem 0.5rem' }}
       onClick={() => setMode({ type: 'playlist', id: id })}
     >
-      <div className='flex flex-row items-center justify-between w-full h-full p-2 gap-2'>
+      <div className='flex flex-row items-center justify-between w-full h-full gap-2'>
         <div className='relative  ml-2 rounded-md overflow-hidden bg-neutral-800'>
           {art ? (
             <Image src={art} alt={title} height={48} width={48} className='object-cover' />
