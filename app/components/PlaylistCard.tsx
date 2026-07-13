0635614b5b4d748a0f5bc5186ba70bbeb045a24a
dawndewaay <dawndewaay@gmail.com>
2026-07-12 20:21:26 -0500

    Nice

--- app/components/PlaylistCard.tsx
+++ app/components/PlaylistCard.tsx
@@ -17,18 +17,21 @@ const PlaylistCard = ({
     <motion.div
       className='h-14 w-full rounded-lg flex items-center justify-center cursor-pointer'
       initial={false}
-      animate={{ padding: '0.5rem 0.5rem', opacity: 0.9, backgroundColor: '#121212' }}
-      whileHover={{ padding: '0.7rem 0.5rem' }}
+      animate={{ opacity: 0.9, backgroundColor: '#121212' }}
       onClick={() => setMode({ type: 'playlist', id: id })}
     >
-      <div className='flex flex-row items-center justify-between w-full h-full gap-2'>
-        <div className='relative  ml-2 rounded-md overflow-hidden bg-neutral-800'>
+      <motion.div
+        className='flex flex-row items-center justify-between w-full h-full gap-2'
+        animate={{ padding: '0.75rem 0.75rem' }}
+        whileHover={{ padding: '1rem 0.75rem' }}
+      >
+        <div className='relative rounded-md overflow-hidden bg-neutral-800'>
           {art ? (
             <Image src={art} alt={title} height={48} width={48} className='object-cover' />
           ) : null}
         </div>
         <div className='text-white font-CircularBold text-lg w-full text-start'>{title}</div>
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
