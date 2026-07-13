5ba6a4d8276ac13c1fee7d3ab6555b3544f278b6
dawndewaay <dawndewaay@gmail.com>
2026-07-12 22:37:07 -0500

    hm

--- app/components/SongCard.tsx
+++ app/components/SongCard.tsx
@@ -4,14 +4,12 @@ import Image from 'next/image';
 const SongCard = ({ name, art }: { name: string; art: string }) => {
   return (
     <motion.div
-      className='h-96 p-4 rounded-3xl flex flex-col items-center justify-start'
+      className='h-96 w-auto p-4 rounded-3xl flex flex-col items-center justify-start'
       initial={false}
       animate={{ backgroundColor: '#121212' }}
     >
-      <div className='relative w-64 aspect-square rounded-xl overflow-hidden'>
-        {art ? (
-          <Image src={art} alt={name ?? ''} fill  className='object-cover' />
-        ) : null}
+      <div className='relative w-80 aspect-square rounded-xl overflow-hidden'>
+        {art ? <Image src={art} alt={name ?? ''} fill className='object-cover' /> : null}
       </div>
       <div className='text-white font-CircularBold text-lg mt-4'>{name}</div>
     </motion.div>
