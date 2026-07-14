a28964b25686c97075d301f7742135430ed8265a
DonDeWaay <dondewaay@gmail.com>
2026-07-14 14:07:38 -0500

    better

--- app/components/Playlist.tsx
+++ app/components/Playlist.tsx
@@ -232,7 +232,7 @@ const Playlist = ({ id, setMode }: { id: string; setMode: (mode: Mode) => void }
                   style={{ zIndex: playlist.tracks.length - index }}
                   initial={false}
                   animate={{
-                    x: isPast ? -600 : 0,
+                    x: isPast ? -200 : 0,
                     y: clamped * 12,
                     rotate: isPast ? -15 : 0,
                     scale: 1 - clamped * 0.04,
