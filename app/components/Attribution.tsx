5025d32198e9cb78f4341d5ec06e9df44780d645
dawndewaay <dawndewaay@gmail.com>
2026-07-14 16:31:35 -0500

    Got it

--- app/components/Attribution.tsx
+++ app/components/Attribution.tsx
@@ -9,7 +9,7 @@ const Attribution = () => {
   return (
     <div className='fixed right-4 bottom-4 z-50'>
       <div
-        className='text-white text-sm hover:underline cursor-pointer'
+        className='text-white hover:underline cursor-pointer opacity-0 md:opacity-100'
         onClick={() => openInNewTab('https://dawndewaay.dev/')}
       >
         © {new Date().getFullYear()} Dawn DeWaay III {'<3'}
